edb763febe63fe120abafa82e99b3755c84e54e4
lukhaza <ntut@hotmail.com>
2020-04-11 18:29:23 +0200

    remove material from routing module

--- angularfrontend/src/app/app-routing.module.ts
+++ angularfrontend/src/app/app-routing.module.ts
@@ -4,20 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {
-  MatToolbarModule,
-  MatMenuModule,
-  MatIconModule,
-  MatButtonModule,
-  MatTableModule,
-  MatDividerModule,
-  MatProgressSpinnerModule,
-  MatInputModule,
-  MatCardModule,
-  MatSlideToggleModule,
-  MatSelectModule,
-  MatOptionModule
-} from '@angular/material';
 import { LoginComponent } from './login/login.component';
 
 
@@ -36,17 +22,6 @@ const routes: Routes = [{
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTableModule,
-    MatDividerModule,
-    MatProgressSpinnerModule,
-    MatInputModule,
-    MatCardModule,
-    MatSlideToggleModule,
-    MatSelectModule,
-    MatOptionModule,]
+  ]
 })
 export class AppRoutingModule { }
